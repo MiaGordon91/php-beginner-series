f15ab82255e134492b354d1a4f2cc67307bf9a0c
Mia Gordon <mia.gordon@digital.justice.gov.uk>
2022-01-24 13:53:30 +0000

    created contacts.php and extracted html to own pages

--- index.php
+++ index.php
@@ -1,116 +1,15 @@
 <?php
 
-$petsJson = file_get_contents('./resources/pets.json');
-$pets = json_decode($petsJson, true);
+require 'layout/header.php';
+require './lib/functions.php';
 
-$pet1 = array(
-    'name' => 'Dior',
-    'age' => '3 years',
-    'breed' => 'Dalmatian',
-    'weight' => 9,
-    'bio' => 'Loves to sleep',
-    'filename' => 'pet1.png'
-);
-
-$pet2 = array(
-    'name' => 'Rex',
-    'age' => '3 years',
-    'breed' => 'Cockapoo',
-    'weight' => 9,
-    'bio' => 'Loves to sleep',
-    'filename' => 'pet2.png'
-);
-
-$pet3 = array(
-    'name' => 'Cookie',
-    'age' => '3 years',
-    'breed' => 'Pug',
-    'weight' => 9,
-    'bio' => 'Loves to sleep',
-    'filename' => 'pet3.png'
-);
-
-$dexter = array(
-    'name' => 'Dexter',
-    'age' => '3 years',
-    'breed' => 'Pug',
-    'weight' => 9,
-    'bio' => 'Loves to sleep',
-    'filename' => 'pet3.png'
-);
-
-
-$pets = array($pet1, $pet2, $pet3, $dexter);
-    var_dump($pets);die();
-
+$pets = get_pets();
 $message = 'All the love, none of the crap!';
 $pupCount = count($pets);
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="shortcut icon" href="favicon.ico">
 
-    <title>AirPupNMeow.com: All the love, none of the crap!</title>
 
-    <link href="css/bootstrap.css" rel="stylesheet">
-    <link href="css/main.css" rel="stylesheet">
-
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-    <script src="js/html5shiv.js"></script>
-    <script src="js/respond.min.js"></script>
-    <![endif]-->
-</head>
-
-<body>
-
-    <div class="navbar navbar-inverse navbar-fixed-top">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">AirPupNMeow</a>
-            </div>
-            <div class="navbar-collapse collapse">
-                <ul class="nav navbar-nav">
-                    <li class="active"><a href="#">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#contact">Contact</a></li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="#">Action</a></li>
-                            <li><a href="#">Another action</a></li>
-                            <li><a href="#">Something else here</a></li>
-                            <li class="divider"></li>
-                            <li class="dropdown-header">Nav header</li>
-                            <li><a href="#">Separated link</a></li>
-                            <li><a href="#">One more separated link</a></li>
-                        </ul>
-                    </li>
-                </ul>
-                <form class="navbar-form navbar-right">
-                    <div class="form-group">
-                        <input type="text" placeholder="Email" class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <input type="password" placeholder="Password" class="form-control">
-                    </div>
-                    <button type="submit" class="btn btn-success">Sign in</button>
-                </form>
-            </div>
-            <!--/.navbar-collapse -->
-        </div>
-    </div>
 
     <div class="jumbotron">
         <div class="container">
@@ -136,29 +35,23 @@ $pupCount = count($pets);
                     <blockquote class="pet_info">
                         <span class="label label-info"><?php echo $pet['breed']; ?> </span>
                         <br>
+                        <?php
+                        if (!array_key_exists('weight', $pet) || $pet['weight'] == "") {
+                            echo 'Unknown';
+                        } else
+                            echo $pet['weight']; ?> lbs,
+
                         <?php echo $pet['age']; ?>
-                        <?php echo $pet['weight']; ?> lbs
+
                     </blockquote>
-
                     <p> 
-                    <?php echo $pet['bio']; ?> 
+                        <?php echo $pet['bio']; ?>
                     </p>
-
-                    </div>
+                 </div>
                     <?php } ?>
             </div>
         <hr>
 
+<?php
+   require 'layout/footer.php';
 
-
-
-                <footer>
-            <p>&copy; AirPupNMeow.com</p>
-        </footer>
-    </div>
-    <!-- /container -->
-
-    <script src="js/jquery.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-</body>
-</html>
---
+++ lib/functions.php
@@ -0,0 +1,12 @@
+<?php
+
+//require './data/pets.json';
+
+function get_pets()
+{
+    $petsJson = file_get_contents('./data/pets.json');
+    $pets = json_decode($petsJson, true);
+
+    return $pets;
+}
+
